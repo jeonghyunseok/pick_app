cacec31521515be4c5255a87f2b527ac0668dfe3
jeonghyunseok <jhs92043@gmail.com>
2023-11-06 00:06:02 +0900

    feat. reset point purple

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.purple,
       body: Center(
         child: Text(
           'PICK!',
